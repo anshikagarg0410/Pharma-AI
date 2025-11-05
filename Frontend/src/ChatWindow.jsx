682935447f63c79cd601841a06129d62033f73b5
unknown <khushi096btcseai24@igdtuw.ac.in>
2025-11-05 16:09:10 +0530

    Changes in Styling

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,3 +1,48 @@
+// import "./ChatWindow.css";
+// // Import icons from react-icons
+// import { FiPlus, FiImage, FiUpload } from "react-icons/fi";
+// import { TfiCommentAlt } from "react-icons/tfi";
+
+// function ChatWindow() {
+//   return (
+//     <>
+//     <div className="navbar">
+//       <span>Pharma AI</span>
+//       <div className="userIconDiv">
+//         <span className="userIcon"><i class="fa-solid fa-user"></i></span>
+//       </div>
+//     </div>
+//     <div className="heading">Welcome to Pharma AI</div>
+//     <div className="subheading">Transforming Pharmaceutical Research Through Agentic Intelligence</div>
+//     <div>
+//       <div className="chat-bar">
+//          <input
+//           type="text"
+//           className="chat-input"
+//           placeholder="How can I help you today?"
+//         />
+//         <div className="left-icons">
+//           <button className="icon-button">
+//             <FiPlus size={20} />
+//           </button>
+//           <button className="icon-button">
+//             <FiImage size={20} />
+//           </button>
+//           <button className="icon-button">
+//             <TfiCommentAlt size={20} />
+//           </button>
+//            <button className="icon-button file-upload-button">
+//           {/* <FiUpload size={20} /> */}
+//           <i class="fa-solid fa-paper-plane"></i>
+//         </button>
+//         </div>
+//         </div>
+//     </div>
+//     </>
+//   );
+// }
+
+// export default ChatWindow;
 import "./ChatWindow.css";
 // Import icons from react-icons
 import { FiPlus, FiImage, FiUpload } from "react-icons/fi";
@@ -5,40 +50,43 @@ import { TfiCommentAlt } from "react-icons/tfi";
 
 function ChatWindow() {
   return (
-    <>
-    <div className="navbar">
-      <span>Pharma AI</span>
-      <div className="userIconDiv">
-        <span className="userIcon"><i class="fa-solid fa-user"></i></span>
+    <div className="chat-container">
+      <div className="navbar">
+        <span>Pharma AI</span>
+        <div className="userIconDiv">
+          <span className="userIcon"><i className="fa-solid fa-user"></i></span>
+        </div>
+      </div>
+
+      <div className="chat-content">
+        <div className="heading">Welcome to Pharma AI</div>
+        <div className="subheading">Transforming Pharmaceutical Research Through Agentic Intelligence</div>
+      </div>
+      <div className="chat-input-area">
+        <div className="chat-bar">
+           <input
+            type="text"
+            className="chat-input"
+            placeholder="How can I help you today?"
+          />
+          <div className="left-icons">
+            <button className="icon-button">
+              <FiPlus size={20} />
+            </button>
+            <button className="icon-button">
+              <FiImage size={20} />
+            </button>
+            <button className="icon-button">
+              <TfiCommentAlt size={20} />
+            </button>
+             <button className="icon-button file-upload-button">
+            {/* <FiUpload size={20} /> */}
+            <i className="fa-solid fa-paper-plane"></i>
+          </button>
+          </div>
+        </div>
       </div>
     </div>
-    <div className="heading">Welcome to Pharma AI</div>
-    <div className="subheading">Transforming Pharmaceutical Research Through Agentic Intelligence</div>
-    <div>
-      <div className="chat-bar">
-         <input
-          type="text"
-          className="chat-input"
-          placeholder="How can I help you today?"
-        />
-        <div className="left-icons">
-          <button className="icon-button">
-            <FiPlus size={20} />
-          </button>
-          <button className="icon-button">
-            <FiImage size={20} />
-          </button>
-          <button className="icon-button">
-            <TfiCommentAlt size={20} />
-          </button>
-           <button className="icon-button file-upload-button">
-          {/* <FiUpload size={20} /> */}
-          <i class="fa-solid fa-paper-plane"></i>
-        </button>
-        </div>
-        </div>
-    </div>
-    </>
   );
 }
 
